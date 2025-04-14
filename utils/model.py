b42451fc52dfdb2f665d997bddc5633ac451de60
programator3001 <oleknagorka@gmail.com>
2025-04-14 17:20:52 +0200

    Do sprawdzenia

--- utils/model.py
+++ utils/model.py
@@ -1,5 +1,5 @@
 users: list = [
-    {"name": "Bernard", "location": "Ełk", "posts": 400},
+    {"name": "Aleksander", "location": "Skierniewice", "posts": 400},
     {"name": "Krzysztof", "location": "Białobrzegi", "posts": 500},
     {"name": "Maja", "location": "Świecie", "posts": 300},
     {"name": "Zuzanna", "location": "Radzyń_Podlaski", "posts": 700},
