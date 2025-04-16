f5da51a0e91711eb61ef2bab4a90b461f5cd8e63
programator3001 <oleknagorka@gmail.com>
2025-04-16 10:56:36 +0200

    wip: new function: add new user

--- controller.py
+++ controller.py
@@ -1,3 +1,9 @@
 def get_user_info(users_data: list) -> None:
     for user in users_data:
         print(f"Twój znajomy {user['name']}! z miejscowosci {user['location']} opublikował {user['posts']} postów")
+
+        def add_user(users_data: list) -> None:
+            user_name = input("podaj imie nowego użytkownika: ")
+            user_location = input("podaj lokalizacje nowego znajomego: ")
+            user_posts = int(input("podaj liczbe postów postów: "))
+            users_data.append({"name": user_name, "location": user_location, "posts": user_posts})
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from utils.model import users
 from utils.controller import get_user_info
 from utils.model import users
-from utils.controller import get_user_info
+from utils.controller import get_user_info, add_user
+
 
 def main():
     while True:
@@ -9,10 +10,12 @@ def main():
         print("===============MENU================")
         print("0 - zakończ program")
         print("1 - pokaż co u znajomych")
+        print("2 - dodaj nowego znajomego")
         print("===================================")
         choice = input("wybierz opcje menu: ")
         if choice == "0": break
         if choice == "1": get_user_info(users)
+        if choice == "2": add_user(users)
     get_user_info(users)
 
 
