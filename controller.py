5ff8485c80f6e6e817a13f866a24725ea891a844
programator3001 <oleknagorka@gmail.com>
2025-04-16 11:13:13 +0200

    wip: new function: remove user

--- controller.py
+++ controller.py
@@ -7,3 +7,10 @@ def get_user_info(users_data: list) -> None:
             user_location = input("podaj lokalizacje nowego znajomego: ")
             user_posts = int(input("podaj liczbe postów postów: "))
             users_data.append({"name": user_name, "location": user_location, "posts": user_posts})
+
+            def remove_user(users_data: list) -> None:
+
+                user_name = input("podaj imię użytkownika do usunięcia: ")
+                for user in users_data:
+                    if user["name"] == user_name:
+                        users_data.remove(user)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from utils.model import users
 from utils.controller import get_user_info
 from utils.model import users
-from utils.controller import get_user_info, add_user
+from utils.controller import get_user_info,add_user, remove_user
 
 
 def main():
@@ -10,12 +10,15 @@ def main():
         print("===============MENU================")
         print("0 - zakończ program")
         print("1 - pokaż co u znajomych")
-        print("2 - dodaj nowego znajomego")
+        print("2 - dodaj znajomego")
+        print("3 - usuń znajomego")
         print("===================================")
         choice = input("wybierz opcje menu: ")
         if choice == "0": break
         if choice == "1": get_user_info(users)
         if choice == "2": add_user(users)
+        if choice == "3": remove_user(users)
+
     get_user_info(users)
 
 
