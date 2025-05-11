250ac35aa74d361196e1a0a813a4f5239eb80d46
programator3001 <oleknagorka@gmail.com>
2025-05-11 22:15:41 +0200

    wip: new function: remove user

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
+from random import choice
+
 from utils.model import users
-from utils.controller import get_user_info
-from utils.model import users
-from utils.controller import get_user_info,add_user, remove_user
+from utils.controller import get_user_info, add_users, remove_user
 
 
 def main():
     while True:
 
-        print("===============MENU================")
+        print("==========MENU==========")
         print("0 - zakończ program")
         print("1 - pokaż co u znajomych")
-        print("2 - dodaj znajomego")
+        print("2 - dodaj nowego znajomego")
         print("3 - usuń znajomego")
-        print("===================================")
+        print('========================')
         choice = input("wybierz opcje menu: ")
-        if choice == "0": break
-        if choice == "1": get_user_info(users)
-        if choice == "2": add_user(users)
+        if  choice == "0": break
+        if  choice == "1": get_user_info(users)
+        if choice == "2": add_users(users)
         if choice == "3": remove_user(users)
-
-    get_user_info(users)
-
 
 if __name__ == "__main__":
     main()
--- model.py
+++ model.py
@@ -1,6 +1,7 @@
-users: list = [
-    {"name": "Bernard", "location": "Ełk", "posts": 400},
-    {"name": "Krzysztof", "location": "Białobrzegi", "posts": 500},
-    {"name": "Maja", "location": "Świecie", "posts": 300},
-    {"name": "Zuzanna", "location": "Radzyń_Podlaski", "posts": 700},
+
+users:list = [
+    {"name":"Adrian","location": "Toruń","posts":400},
+    {"name":"Krzysztof","location": "Baiłobrzegi","posts":500},
+    {"name":"Maja","location":" Świecie","posts":300},
+    {"name":"Zuzia","location":" Radzyń_Podlaski","posts":700},
 ]
