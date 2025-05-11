ca1fcc7ad7b60504750bee0f9b7e7157ccb4e057
programator3001 <oleknagorka@gmail.com>
2025-05-11 22:21:37 +0200

    wip: new function: remove user

--- model.py
+++
@@ -1,7 +0,0 @@
-
-users:list = [
-    {"name":"Adrian","location": "Toruń","posts":400},
-    {"name":"Krzysztof","location": "Baiłobrzegi","posts":500},
-    {"name":"Maja","location":" Świecie","posts":300},
-    {"name":"Zuzia","location":" Radzyń_Podlaski","posts":700},
-]
--- utils/controller.py
+++ utils/controller.py
@@ -1,3 +1,21 @@
-def get_user_info(users_data: list) -> None:
+def get_user_info(users_data:list)->None:
+
     for user in users_data:
-        print(f"Twój znajomy {user['name']}! z miejscowosci {user['location']} opublikował {user['posts']} postów")
+        print(f"Twój znajomy {user['name']}! z miejscowości{user["location"]} opublikował {user['posts']} postów")
+
+
+
+
+def add_users(users_data: list)->None:
+
+    users_name=input("podaj imię nowego użytkownika ")
+    users_location=input("podaj lokalizacje nowego znajomego ")
+    users_posts=int(input("podaj liczbe postów: "))
+    users_data.append( {"name":users_name,"location":users_location,"posts":users_posts})
+
+def remove_user(users_data: list)->None:
+
+    user_name=input("podaj imię użytkownika do usuniecia: ")
+    for user in users_data:
+        if user["name"] == user_name:
+            users_data.remove(user)
